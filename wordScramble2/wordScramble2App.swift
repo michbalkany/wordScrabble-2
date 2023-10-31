90edf3ff15a32dec9576540a1e009a9441075940
mich balkany <askmitchb@gmail.com>
2023-10-31 10:27:58 -0400

    Initial Commit

---
+++ wordScramble2/wordScramble2App.swift
@@ -0,0 +1,17 @@
+//
+//  wordScramble2App.swift
+//  wordScramble2
+//
+//  Created by Mich balkany on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct wordScramble2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
